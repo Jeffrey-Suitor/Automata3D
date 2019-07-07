93c3cbf29df287291e1235b557f014b69b96d885
Jeffrey-Suitor <jeffreysuitor117@gmail.com>
2019-07-06 23:03:13 -0400

    Need to create custom exceptions. Need to do print run out and print failure. All other aspects have been completed. Beginning testing soon.

--- machineVision.py
+++ machineVision.py
@@ -2,6 +2,13 @@ from statistics import mean
 import numpy as np
 import cv2
 import time
+import itertools
+import logging as log
+
+# Logger {{{
+log.basicConfig(level=log.INFO)
+logger = log.getLogger(__name__)
+#}}}
 
 # Instructions {{{
 """
@@ -35,17 +42,20 @@ def getCircles(image, circleType):
                                    param2=4,  # Base detection
                                    minRadius=6,  # min rad
                                    maxRadius=10)  # max rad
+        if circles is None:
+            log.debug("No circles nozzle markers found")
 
     if circleType == "nozzle":
         circles = cv2.HoughCircles(image,
                                    cv2.HOUGH_GRADIENT,
                                    1,  # Type of detection
-                                   50,  # Distance between points
+                                   100,  # Distance between points
                                    param1=130,  # Canny threshold
                                    param2=12,  # Base detection
                                    minRadius=6,  # min rad
                                    maxRadius=10)  # max rad
-
+        if circles is None:
+            log.debug("No circles nozzle markers found")
     return circles
 #}}}
 
@@ -60,13 +70,21 @@ def getAverageCircles(frameList, markerType, markers=2):
     for frame in frameList:
         circles = getCircles(frame, markerType)
 
-        if circles is not None: #If there are circles
+        if circles is not None:  # If there are circles
             circles = np.int16(np.around(circles))
-            circles = circles[0] #Remove the one dimensional list
-            if len(circles) == markers: #If we find the right number of circles
-                circles = sorted(circles, key=lambda x:x[:][0]) #Sort circles by x
-                for i in range(markers):
-                    averageList[i].append(circles[i])
+            circles = circles[0]  # Remove the one dimensional list
+            circles = sorted(circles, key=lambda x:x[:][0])  # Sort circles by x
+            if len(circles) == markers:  # If we find the right number of circles
+                if abs(circles[0][1] - circles[1][1]) < 15 and abs(circles[0][0] - circles[1][0]) > 100:
+                    averageList[0].append(circles[0])
+                    averageList[1].append(circles[1])
+
+            elif len(circles) > markers:
+                for c1, c2, in itertools.product(circles, circles):
+                    if abs(c1[1] - c2[1]) < 15 and abs(c1[0] - c2[0]) > 100 and c1[0] != c2[0]:
+                        averageList[0].append(c1)
+                        averageList[1].append(c2)
+                        break
 
     # }}}
 
@@ -74,7 +92,7 @@ def getAverageCircles(frameList, markerType, markers=2):
 
         # convert to list {{{
         for i in range(len(averageList)):
-            tempList = np.mean(averageList[i], axis=0) #Get the average of the list
+            tempList = np.mean(averageList[i], axis=0)  # Get the average of the list
             returnList.append(tempList.tolist())
         # }}}
 
@@ -82,7 +100,7 @@ def getAverageCircles(frameList, markerType, markers=2):
         try:
             for i in range(len(returnList)):
                 for j in range(len(returnList[i])):
-                    returnList[i][j] = int(returnList[i][j]) #Convert all values to int
+                    returnList[i][j] = int(returnList[i][j])  # Convert all values to int
             return returnList
         except TypeError:
             return
@@ -98,7 +116,7 @@ def cropImage(image, bedMarkers=None, nozzleMarkers=None):
 
         yMin = min(map(lambda x: x[1], markers))
         yMax = max(map(lambda x: x[1], markers))
-        return image[:yMax-30, xMin:xMax], [0, yMax-30, xMin, xMax]
+        return image[:yMax-30, :]
 
     if bedMarkers and nozzleMarkers:
         markers = []
@@ -108,74 +126,55 @@ def cropImage(image, bedMarkers=None, nozzleMarkers=None):
             markers.append(marker)
 
 
-        yMin = min(map(lambda x: x[0], markers))
-        yMax = max(map(lambda x: x[0], markers))
+        xMin = min(map(lambda x: x[0], markers))
+        xMax = max(map(lambda x: x[0], markers))
 
-        xMin = min(map(lambda x: x[1], markers))
-        xMax = max(map(lambda x: x[1], markers))
-        return image[xMin:xMax, yMin:yMax],  [yMin, yMax, xMin, xMax]
+        yMin = min(map(lambda x: x[1], markers))
+        yMax = max(map(lambda x: x[1], markers))
+        return image[yMin:yMax, xMin:xMax]
 
 # }}}
 
 # locPrintHead {{{
 def locPrintHead(nozMarkers):
-    x = int((nozMarkers[1][0] - nozMarkers[0][0]) / 2 + nozMarkers[0][0] - 30)
-    y = int(nozMarkers[0][1] + 30)
+    x = int((nozMarkers[1][0] - nozMarkers[0][0]) / 2 + nozMarkers[0][0] - 17)
+    y = int(nozMarkers[0][1] + 25)
     z = 0
     return [x, y, z]
 #}}}
 
 # markerImage {{{
 def markerImage(image):
-    bedMakers = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    nozzleMarkers = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    bedLower, bedUpper, nozzleLower, nozzleUpper = getHsvValues()
-    bedMask = cv2.inRange(bedMakers, bedLower, bedUpper)
-    nozzleMask = cv2.inRange(nozzleMarkers, nozzleLower, nozzleUpper)
-    return bedMask, nozzleMask
-#}}}
+    bedMrk = nozMrk = prtMrk = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    bedL, bedU, nozL, nozU, prtL, prtU = getHsvValues()
+
+    bedMask = cv2.inRange(bedMrk, bedL, bedU)
+    nozzleMask = cv2.inRange(nozMrk, nozL, nozU)
+    partMask = cv2.inRange(prtMrk, prtL, prtU)
+
+    return bedMask, nozzleMask, partMask
+# }}}
 
 # getHsvValues {{{
 def getHsvValues():
-    #values = input("Please input the RGB value seperated by a space : ")
-    values = "130 200 107"
-    valueList = values.split()
-    for i in range(len(valueList)):
-        valueList[i] = int(valueList[i])
-    BGRValues = np.uint8([[valueList]])
-    hsv_values = cv2.cvtColor(BGRValues, cv2.COLOR_RGB2HSV)
-
-    bedLower = hsv_values.copy()
-    bedUpper = hsv_values.copy()
-
-    nozzleLower = hsv_values.copy()
-    nozzleUpper = hsv_values.copy()
-
-    bedLower[0][0][0] = 150
-    bedLower[0][0][1] = 200
-    bedLower[0][0][2] = 200
-
-    bedUpper[0][0][0] = 255
-    bedUpper[0][0][1] = 255
-    bedUpper[0][0][2] = 255
-
-    nozzleLower[0][0][0] = 45
-    nozzleLower[0][0][1] = 25
-    nozzleLower[0][0][2] = 25
-
-    nozzleUpper[0][0][0] = 255
-    nozzleUpper[0][0][1] = 255
-    nozzleUpper[0][0][2] = 255
-
-    return bedLower, bedUpper, nozzleLower, nozzleUpper
+    bedLower = np.uint8([[150, 150, 150]])
+    bedUpper = np.uint8([[255, 255, 255]])
+    nozzleLower = np.uint8([[47, 35, 35]])
+    nozzleUpper = np.uint8([[252, 255, 255]])
+    partLower = np.uint8([[82, 50, 40]])
+    partUpper = np.uint8([[110, 200, 200]])
+    return bedLower, bedUpper, nozzleLower, nozzleUpper, partLower, partUpper
 # }}}
 
 # findCircles {{{
-def findCircles(frameList, markerImage, markerType, averageValue=10):
+def findCircles(frameList, markerType, averageValue=10):
     averagedMarkers = None
     if len(frameList) > averageValue:
         averagedMarkers = getAverageCircles(frameList, markerType)
         del frameList[0]
+    else:
+        log.debug("Gathering {} frames".format(markerType))
     if averagedMarkers is not None:
         return averagedMarkers
 # }}}
@@ -191,65 +190,88 @@ def drawCircles(image, bedMarkers=None, nozzleMarkers=None, printHead=None):
             cv2.circle(image, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
             cv2.circle(image, (circle[0], circle[1]), 2, (0, 0, 255), 2)
     if printHead is not None:
-        cv2.circle(output, (printHead[0], printHead[1]), 3, (255, 0, 0), 2)
+        cv2.circle(image, (printHead[0], printHead[1]), 3, (255, 0, 0), 2)
     return image
 
 #}}}
 
+# filamentRunOut {{{
+def filamentRunout(printHead, partBox):
+    _, partY, _, _ = partBox
+    _, pHeadY, _ = printHead
+    if partY - pHeadY > 20:
+        # TODO: Add a custom exception here for print run out
 
-if __name__ == '__main__':
+#}}}
 
+# main {{{
+def main():
     # setup {{{
     cap = cv2.VideoCapture("/dev/Cameras/MachineVision")
     bedFrames = []
     nozzleFrames = []
+    partFrames = []
     averagedListValues = None
     avgBed = None
     avgNozzle = None
-
+    printHead = None
+    log.info("Setup complete")
     # }}}
 
     while True:
 
-        # getImage {{{
-        ret, image = cap.read()
-        image = cv2.flip(image, 0)
-        output = image.copy()
-        output2 = image.copy()
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        bedMask, nozzleMask = markerImage(image)
-        bedImage = cv2.GaussianBlur(bedMask, (5, 5), 0)
-        nozzleImage = cv2.GaussianBlur(nozzleMask, (5, 5), 0)
-        # }}}
+        try:
+            # getImage {{{
+            ret, image = cap.read()
+            image = cv2.flip(image, 0)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            bedMask, nozzleMask, partMask = markerImage(image)
+            bedImage = cv2.GaussianBlur(bedMask, (5, 5), 0)
+            nozzleImage = cv2.GaussianBlur(nozzleMask, (5, 5), 0)
+            partImage = cv2.GaussianBlur(partMask, (5, 5), 0)
+            log.debug("Capture complete")
+            # }}}
 
-        bedFrames.append(bedImage)
-        nozzleFrames.append(nozzleImage)
-        avgBed = None
-        avgNozzle = None
-        printHead = None
-        avgBed = findCircles(bedFrames, bedImage, "bed")
-        if avgBed:
-            nozzleOutput, s = cropImage(nozzleImage, bedMarkers=avgBed)
-            avgNozzle = findCircles(nozzleFrames, nozzleOutput, "nozzle")
-            kernel = np.ones((10,10),np.uint8)
-            opening = cv2.morphologyEx(nozzleOutput, cv2.MORPH_OPEN, kernel)
-            avgNozzle = getCircles(nozzleOutput, "nozzle")
-            nozzleOutput = output[s[0]:s[1], s[2]:s[3]]
-            nozzleOutput = cv2.bitwise_and(nozzleOutput, nozzleOutput, mask=nozzleMask[s[0]:s[1], s[2]:s[3]])
-            try:
-                avgNozzle = avgNozzle[0]
-            except TypeError:
-                pass
-            if len(avgNozzle) != 2:
-                print(len(avgNozzle))
-                drawCircles(nozzleOutput, avgBed, avgNozzle, printHead)
-                cv2.imshow("output", nozzleOutput)
-                cv2.waitKey(0)
-        #if avgNozzle:
-            #processingImage = cropImage(output, bedMarkers=avgBed, nozzleMarkers=avgNozzle)
-            #printHead = locPrintHead(avgNozzle)
-        else:
-            print("failed")
+            # Bed Finding {{{
+            bedFrames.append(bedImage)
+            avgBed = findCircles(bedFrames, "bed")
+            # }}}
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Nozzle Finding{{{
+            if avgBed:
+                log.debug("Bed found")
+                nozzleProcess = cropImage(nozzleImage, bedMarkers=avgBed)
+                kernel = np.ones((1,1),np.uint8)
+                nozzleProcess = cv2.morphologyEx(nozzleProcess, cv2.MORPH_OPEN, kernel)
+                nozzleFrames.append(nozzleProcess)
+                avgNozzle = findCircles(nozzleFrames, "nozzle")
+                # }}}
+
+            # Part Finding {{{
+            if avgNozzle:  # If we find the bed and the nozzle find the part
+                log.debug("Nozzle found")
+                printHead = locPrintHead(avgNozzle)
+                partProcess = cv2.medianBlur(partImage, 51)
+                contours, hierarchy = cv2.findContours(partProcess, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                part = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+                bBox = cv2.boundingRect(part)
+                cv2.rectangle(image, (bBox[0], bBox[1]), (bBox[0]+bBox[2], bBox[1]+bBox[3]), (0, 255, 0), 2)
+            # }}}
+
+            # Error Detection {{{
+
+
+            # }}}
+
+
+            drawCircles(image, avgBed, avgNozzle, printHead)
+            cv2.imshow("3D Print", image)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except cv2.error:
+            pass
+#}}}
+
+if __name__ == '__main__':
+    main()
